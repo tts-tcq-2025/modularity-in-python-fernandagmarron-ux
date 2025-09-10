3aba01198a51b46d6597e105cdd0647692922718
fernandagmarron-ux <fernandagmarron@gmail.com>
2025-09-10 09:41:27 -0600

    Update Colors.py

--- Colors.py
+++ Colors.py
@@ -3,11 +3,3 @@
 MAJOR_COLORS = ['White', 'Red', 'Black', 'Yellow', 'Violet']
 MINOR_COLORS = ["Blue", "Orange", "Green", "Brown", "Slate"]
 
-
-def get_all_color_combinations():
-    combinations = []
-    for major in MAJOR_COLORS:
-        for minor in MINOR_COLORS:
-            combinations.append(f"{major} {minor}")
-    return combinations
-
